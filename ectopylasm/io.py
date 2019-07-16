a3975dfa3c943ec575874127c44786a33c5c37eb
E. G. Patrick Bos <egpbos@gmail.com>
2019-07-16 11:20:26 +0200

    fix strict linting issues (docstrings)

--- ectopylasm/io.py
+++ ectopylasm/io.py
@@ -1,3 +1,5 @@
+"""Functions for file handling."""
+
 import plyfile
 import pandas as pd
 import vaex as vx
@@ -8,21 +10,25 @@ logger.setLevel(logging.DEBUG)
 
 
 def load_plyfile(filename):
+    """Load a PLY file."""
     plydata = plyfile.PlyData.read(filename)
     return plydata
 
 
 def vertex_dict_from_plyfile(filename):
+    """Load vertices from plyfile and return as dict."""
     plydata = load_plyfile(filename)
     xyz = dict(x=plydata['vertex']['x'], y=plydata['vertex']['y'], z=plydata['vertex']['z'])
     return xyz
 
 
 def pandas_vertices_from_plyfile(filename):
+    """Load vertices from plyfile and return as pandas DataFrame."""
     xyz = vertex_dict_from_plyfile(filename)
     return pd.DataFrame(xyz)
 
 
 def vaex_vertices_from_plyfile(filename):
+    """Load vertices from plyfile and return as vaex DataFrame."""
     xyz = vertex_dict_from_plyfile(filename)
     return vx.from_dict(xyz)
