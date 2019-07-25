69bfca65b9ff88cb5e8211b1430705fff8c734df
E. G. Patrick Bos <egpbos@gmail.com>
2019-07-25 16:36:41 +0200

    add source code to docs

--- docs/conf.py
+++ docs/conf.py
@@ -35,7 +35,7 @@ sys.path.insert(0, os.path.abspath(os.path.join(here, '..')))
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
 extensions = ['sphinx.ext.autodoc', 'sphinx.ext.napoleon',
-              'sphinx.ext.intersphinx']
+              'sphinx.ext.intersphinx', 'sphinx.ext.viewcode']
 
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ['_templates']
